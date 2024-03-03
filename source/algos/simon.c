da935979fb4fa96187ce55c3858b1862ba3c5817
Reini Urban <reini.urban@gmail.com>
2024-03-03 12:53:44 +0100

    simon: fix stack-use-after-return
    needs globals cells[], not thread-safe

--- source/algos/simon.c
+++ source/algos/simon.c
@@ -15,11 +15,16 @@
  *
  * contact the authors at: faro@dmi.unict.it, thierry.lecroq@univ-rouen.fr
  * download the tool at: http://www.dmi.unict.it/~faro/smart/
+ *
+ * Constraints: not thread-safe
  */
 
 #include "include/define.h"
 #include "include/main.h"
 #include "include/AUTOMATON.h"
+
+// TODO prove the bounded size (2)
+static struct _cell s_cells[2 * M_CUTOFF];
 
 int getTransitionSimon(unsigned char *x, int m, int p, List L[], char c) {
   List cell;
@@ -40,8 +45,6 @@ int getTransitionSimon(unsigned char *x, int m, int p, List L[], char c) {
 
 void setTransitionSimon(int p, int q, List L[], const int m, const int j) {
   List cell;
-  // TODO prove the bounded size (2)
-  struct _cell s_cells[2 * M_CUTOFF];
 
   if (m > M_CUTOFF) {
     cell = (List)malloc(sizeof(struct _cell));
