b8a103a79a0da7d6bc065f1455f253ff4bf6d830
Reini Urban <reini.urban@gmail.com>
2024-03-23 11:15:19 +0100

    sanitizer results

--- kill-tests.sh
+++ kill-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 smart=`ps xw | perl -anle'$F[4] eq "./smart" && print $F[0]'`
-while [ $smart -gt 0 ]
+while test -n "$smart" && test $smart -gt 0
 do
     sleep 2s
-    ps xw | perl -anle'$F[4]=~m{^\./bin/} && $F[5] eq "shared" && $F[3]!~/^0:0[0-5]/ && kill("TERM",$F[0])'
+    ps xw | perl -anle'$F[4]=~m{^\./bin/} && $F[5] eq "shared" && $F[3]!~/^0:0[0-5]/ &&
+print($_) && kill("TERM",$F[0])'
     smart=`ps xw | perl -anle'$F[4] eq "./smart" && print $F[0]'`
 done
