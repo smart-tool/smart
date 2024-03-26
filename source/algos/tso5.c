8e97d118cd3f31aa8b43d788eb28ae9a0f2b3807
Reini Urban <reini.urban@gmail.com>
2024-03-26 10:42:54 +0100

    tso5: fix wrong B[] init, add asserts
    fixed the tests, but it still underflows T

--- source/algos/tso5.c
+++ source/algos/tso5.c
@@ -1,4 +1,6 @@
 // optimized word-wise popcount
+// Constraints: requires m<=64
+
 #include "include/define.h"
 #include "include/main.h"
 #include "include/search_large.h"
@@ -8,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// Constraints: requires m<=64
+#include <assert.h>
 
 // searching
 int search(unsigned char *P, int m, unsigned char *T, int n) {
@@ -26,12 +28,12 @@ int search(unsigned char *P, int m, unsigned char *T, int n) {
 
   BEGIN_PREPROCESSING
   //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
-  memset(B, 0, 256 * 4);
-  mask = ~(uint64_t)0 >> (64 - m);
-  for (j = 0; j < 256; B[j++] = ~(uint64_t)0)
+  memset(B, 0, 256 * 8);
+  mask = ~UINT64_C(0) >> (64 - m);
+  for (j = 0; j < 256; B[j++] = ~UINT64_C(0))
     ;
   for (j = 0; j < m; ++j)
-    B[P[j]] &= ~((uint64_t)1U << j);
+    B[P[j]] &= ~(UINT64_C(1) << j);
   for (j = 0; j < 256; j++)
     B1[j] = (B[j] & mask);
 #ifndef HAVE_POPCOUNT
@@ -45,18 +47,23 @@ int search(unsigned char *P, int m, unsigned char *T, int n) {
   BEGIN_SEARCHING
   T[n + 1] = 255; // sentinel
   for (i = m - 1; i <= n - 1; i += m) {
+    assert(i - 2 >= 0);
+    assert(i + 2 < n);
     if ((D = (B[T[i]] | (B1[T[i - 2]] << 2) | (B1[T[i - 1]] << 1) |
-              (B1[T[i + 1]] >> 1) | (B1[T[i + 2]] >> 2))) != ~(uint64_t)0) {
+              (B1[T[i + 1]] >> 1) | (B1[T[i + 2]] >> 2))) != ~UINT64_C(0)) {
       j = 3;
+      assert(i - j >= 0);
       while ((j < m) && ((D |= ((B1[T[i - j]] << j) | (B1[T[i + j]] >> j))) !=
-                         (~(uint64_t)0)))
+                         (~UINT64_C(0)))) {
         j++;
+        assert(j < m && i - j >= 0);
+      }
 
       // TODO: OUTPUT
 #ifdef HAVE_POPCOUNTLL
       count += POPCOUNT64(~D);
 #else
-      if (D != ~(uint64_t)0) {
+      if (D != ~UINT64_C(0)) {
         count += POPCOUNT16((~D) & 0xffff);
         if (sizeof(D) > 2) {
           count += POPCOUNT16(((~D) >> 16) & 0xffff);
