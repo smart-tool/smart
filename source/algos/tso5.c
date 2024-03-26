ca945e4c4b9203fc09a03837e551758c5ea72979
Reini Urban <reini.urban@gmail.com>
2024-03-26 10:57:59 +0100

    tso5: fixed most out-of bounds accesses

--- source/algos/tso5.c
+++ source/algos/tso5.c
@@ -1,9 +1,10 @@
 // optimized word-wise popcount
-// Constraints: requires m<=64
+// Constraints: requires m>2 && m<=64
 
 #include "include/define.h"
 #include "include/main.h"
 #include "include/search_large.h"
+#include "include/search_small.h"
 
 #include <inttypes.h>
 #include <stdint.h>
@@ -24,6 +25,8 @@ int search(unsigned char *P, int m, unsigned char *T, int n) {
 
   if (m > 64)
     return search_large(P, m, T, n);;
+  if (m < 2)
+    return search_small(P, m, T, n);;
   // memcpy(pat.pat+1, base, m);
 
   BEGIN_PREPROCESSING
@@ -47,16 +50,21 @@ int search(unsigned char *P, int m, unsigned char *T, int n) {
   BEGIN_SEARCHING
   T[n + 1] = 255; // sentinel
   for (i = m - 1; i <= n - 1; i += m) {
-    assert(i - 2 >= 0);
-    assert(i + 2 < n);
-    if ((D = (B[T[i]] | (B1[T[i - 2]] << 2) | (B1[T[i - 1]] << 1) |
-              (B1[T[i + 1]] >> 1) | (B1[T[i + 2]] >> 2))) != ~UINT64_C(0)) {
+    //assert(i - 2 >= 0);
+    //assert(i + 2 < n);
+    if ((D = (B[T[i]] |
+              ((i - 2 >= 0) ? (B1[T[i - 2]] << 2) : 0) |
+              ((i - 1 >= 0) ? (B1[T[i - 1]] << 1) : 0) |
+              (B1[T[i + 1]] >> 1) |
+              ((i + 2 <= n) ? (B1[T[i + 2]] >> 2) : 0))) != ~UINT64_C(0)) {
       j = 3;
       assert(i - j >= 0);
+      assert(i + j < n);
       while ((j < m) && ((D |= ((B1[T[i - j]] << j) | (B1[T[i + j]] >> j))) !=
                          (~UINT64_C(0)))) {
         j++;
         assert(j < m && i - j >= 0);
+        assert(j < m && i + j < n);
       }
 
       // TODO: OUTPUT
